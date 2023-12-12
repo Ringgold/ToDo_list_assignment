2746729c8ce3f13013e2bf3c2f5751349c0da62b
Ringgold <ringgoldlin@yahoo.com>
2023-12-12 01:43:31 -0500

    Add todo list with error message

---
+++ src/TodoList.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Task } from './Task';
+
+var global_id = 0;
+const TodoList: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskText, setNewTaskText] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const addTask = () => {
+    if (newTaskText.trim() === '') {
+      setErrorMessage('Task cannot be empty');
+      return;
+    }
+    setErrorMessage(''); // Clear error message if task is valid
+
+    global_id++;
+    const newTask: Task = {
+      id: global_id,
+      text: newTaskText,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskText('');
+  };
+
+  const toggleTaskCompletion = (taskId: number) => {
+    const updatedTasks = tasks.map(task =>
+      task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId: number) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
+  return (
+    <div>
+      <input
+        title="task"
+        type="text"
+        value={newTaskText}
+        onChange={(e) => setNewTaskText(e.target.value)}
+      />
+      <button onClick={addTask}>➕</button>
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            <input
+                title="checkbox"
+                type="checkbox"
+                id={`task-${task.id}`}
+                className="checkbox-custom"
+                checked={task.isCompleted}
+                onChange={() => toggleTaskCompletion(task.id)}
+            />
+            <label htmlFor={`task-${task.id}`}></label>
+            {task.text}
+            <button onClick={() => deleteTask(task.id)}>🗑️</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TodoList;
